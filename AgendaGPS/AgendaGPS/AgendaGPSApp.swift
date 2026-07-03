1632bd359273969e0ba17cc92a1967657c40b00b
Luis De Lira <delandezleduardo@gmail.com>
2026-07-03 10:03:04 -0600

    chore: initial commit

---
+++ AgendaGPS/AgendaGPS/AgendaGPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaGPSApp.swift
+//  AgendaGPS
+//
+//  Created by Luis Eduardo De Lira Hernández on 03/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AgendaGPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgendaGPS/AgendaGPS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AgendaGPS
+//
+//  Created by Luis Eduardo De Lira Hernández on 03/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

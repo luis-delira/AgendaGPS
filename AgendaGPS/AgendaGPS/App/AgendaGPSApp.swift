358d2bb33166d59fc8ad149a83de0eb1cc8f33bb
Luis De Lira <delandezleduardo@gmail.com>
2026-07-03 11:15:56 -0600

    feat: integrate Firebase SDK

--- AgendaGPS/AgendaGPS/App/AgendaGPSApp.swift
+++ AgendaGPS/AgendaGPS/App/AgendaGPSApp.swift
@@ -1,17 +1,25 @@
-//
-//  AgendaGPSApp.swift
-//  AgendaGPS
-//
-//  Created by Luis Eduardo De Lira Hernández on 03/07/26.
-//
-
 import SwiftUI
+import FirebaseCore // Importamos Firebase
 
+// 1. El AppDelegate para configurar Firebase
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("¡Firebase configurado exitosamente!")
+        return true
+    }
+}
+
+// 2. Tu punto de entrada principal
 @main
 struct AgendaGPSApp: App {
+    // Conectamos el AppDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView() // Esta es la vista por defecto que te creó Xcode
         }
     }
 }
